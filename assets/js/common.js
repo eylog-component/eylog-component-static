4abec7d357a0793f2a428bbfdf161a4fe3d6b83d
eylog-component <110050437+eylog-component@users.noreply.github.com>
2022-07-26 21:43:32 +0530

    Initial Commit

---
+++ assets/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  Prism.highlightAll();
+  $(".mCustomScrollbar-h").mCustomScrollbar({
+    theme:"dark",
+    axis:"x",
+    scrollbarPosition: "outside"
+  });
+});
