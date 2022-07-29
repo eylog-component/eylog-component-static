4e2874ff3edd2c43880962861a8d75dd5dc8c10e
Sushanta Bhowmik <sushantabhowmik@metadesignsolutions.co.uk>
2022-07-29 11:14:44 +0530

    Added multi-select

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,4 +5,18 @@ $(document).ready(function() {
     axis:"x",
     scrollbarPosition: "outside"
   });
+
+  $('#example-getting-started').multiselect({
+    selectAllText: 'All',
+    nonSelectedText: 'Select Nursery/Branch(s)',
+    includeSelectAllOption: true,
+    maxHeight: 140,
+    onInitialized: function(select, container) {
+      $('.multiselect-container.dropdown-menu').addClass('mCustomScrollbar-v');
+      $(".mCustomScrollbar-v").mCustomScrollbar({
+        theme:"dark",
+        axis:"y",
+      });
+    }
+  });
 });
